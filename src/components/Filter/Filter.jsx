7c7cb21a51d8aea089e1cee6230011abcebe0be8
Yevhen-Karpov <goldblogart@gmail.com>
2022-01-23 22:26:19 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { getFilter } from '../../redux/selectors';
 import contactsActions from '../../redux/actions';
 import s from './Filter.module.css';
 
 export default function Filter() {
-  const value = useSelector(state => state.users.filter);
+  const value = useSelector(getFilter);
   const dispatch = useDispatch();
   const onChange = e =>
     dispatch(contactsActions.changeFilter(e.currentTarget.value));
